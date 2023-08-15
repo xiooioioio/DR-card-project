5dc905938f4764cf8926b1636d12d3598e26c87e
digitalready-steven <140424899+digitalready-steven@users.noreply.github.com>
2023-08-15 15:08:02 -0400

    Update R.py

--- R.py
+++ R.py
@@ -11,9 +11,25 @@ class Player():
 
 class Human(Player):
     def __init__(self, deck):
+        super().__init__(deck, [], 0)
         first_half = deck[:len(deck) // 2]  # Get the first half of the deck
         self.hand = first_half[:6]  # Draw six cards from the first half
-        self.score = 0  # Initialize the score to zero
+
+    def choose_card_from_hand(self):
+        print("Your Hand:")
+        for index, card in enumerate(self.hand, 1):
+            print(f"{index}. {card}")
+
+        while True:
+            try:
+                choice_index = int(input("Enter the number corresponding to your choice: ")) - 1
+                if 0 <= choice_index < len(self.hand):
+                    return self.hand.pop(choice_index)
+                else:
+                    print("Invalid choice. Please enter a valid number.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
 
 class Computer(Player):
     def __init__(self, deck):
@@ -40,34 +56,32 @@ computer_player = Computer(second_half)
 print("First Half (Randomized):", first_half)
 print("Second Half (Randomized):", second_half)
 
-# Print the hands of each player
-print("Human's Hand:", human_player.hand)
-print("Computer's Hand:", computer_player.hand)
+# Player's turn
+chosen_card = human_player.choose_card_from_hand()
+print("You chose:", chosen_card)
 
+# Computer's turn
 # Find the highest card in the computer player's hand
 highest_card = None
 for card in computer_player.hand:
     if highest_card is None or card.value > highest_card.value:
         highest_card = card
+print("Computer chose:", highest_card)
 
-print("Computer's Highest Card:", highest_card)
+# ... (previous code remains unchanged) ...
 
+# Player's turn
+chosen_card = human_player.choose_card_from_hand()
+print("You chose:", chosen_card)
 
+# Computer's turn
+computer_highest_card = max(computer_player.hand, key=lambda card: card.value)
+print("Computer chose:", computer_highest_card)
 
-
-
-
-
-
-
-           
-
-
-
-  
- 
-
-
-
-
-
+# Compare the values of the chosen cards to determine the winner
+if chosen_card.value > computer_highest_card.value:
+    print("You win!")
+elif chosen_card.value < computer_highest_card.value:
+    print("Computer wins!")
+else:
+    print("It's a tie!")
